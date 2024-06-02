735e00a8659acd1ed142c4bee3d5d18e6ec44c9a
Kunsguru <kingsleym2024@gmail.com>
2024-06-02 17:07:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+// Simulating live updates
+    setInterval(function() {
+        // Generate random numbers for home, away, and draw
+        var homeScore = Math.floor(Math.random() * 5);
+        
+        var drawScore = Math.floor(Math.random() * 5);
+        
+        var awayScore = Math.floor(Math.random() * 5);
+        
+        // Update the displayed results
+        document.getElementById('homeResult').innerText = homeScore;
+        
+        document.getElementById('drawResult').innerText = drawScore;
+        
+        document.getElementById('awayResult').innerText = awayScore;
+    }, 20000); // Update every 2 seconds
